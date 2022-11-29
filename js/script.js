a67001acd296f86ba667ebbcbe009889ff93ebc0
gerardo Alvarez <1gerardo3@gmail.com>
2022-11-28 17:18:54 -0800

    add starter code

---
+++ js/script.js
@@ -0,0 +1,33 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+var abc = [a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z]
+var nums = [1,2,3,4,5,6,7,8,9,0]
+var spchar =["/,*,-,+,/,?,],%,#,$"]
+
+
+
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+if (){
+  
+}
+
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+
+
+
+// asign var and arreys need 
+// asign functions
+// have it randomize 
+// make it 
